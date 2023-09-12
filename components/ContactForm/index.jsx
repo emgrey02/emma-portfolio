a677f68920ec04ffb5e7b4a4d36dc006ff0c949c
Emma Grey <emgrey02@gmail.com>
2023-09-12 12:26:50 -0400

    See if this works :(

--- components/ContactForm/index.jsx
+++ components/ContactForm/index.jsx
@@ -1,20 +1,40 @@
+import { useRouter } from 'next/router';
+import Button from '../Button';
 import styles from './ContactForm.module.scss';
 
-import Button from '../Button';
-
 export default function ContactForm() {
+    const router = useRouter();
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const myForm = event.target;
+        const formData = new FormData(myForm);
+
+        fetch('/contact', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: new URLSearchParams(formData).toString(),
+        })
+            .then(() => router.push('/form-thankyou'))
+            .catch((error) => alert(error));
+    };
+
     return (
         <div className={styles.container}>
             <form
+                data-netlify="true"
                 name="contact form"
-                method="POST"
+                method="post"
+                action="/pages/form-thankyou"
                 className={styles.form}
+                onSubmit={handleSubmit}
                 id="send-message"
                 aria-label="Send a message to Emma"
-                data-netlify="true"
-                action="/form-thankyou"
             >
-                <input type="hidden" name="contact form" value="contact form" />
+                <input type="hidden" name="form-name" value="contact form" />
                 <label htmlFor="name">name</label>
                 <input id="name" name="name" type="text" required />
 
