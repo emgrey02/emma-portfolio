7532757eb1aa667eab1c269f98502d204669ea1c
emgrey02 <emgrey02@gmail.com>
2025-05-01 15:57:52 -0400

    Tiny fixes

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,20 +1,20 @@
 import Navigation from './components/MainNav';
 import './global.css';
-import styles from './layout.module.scss';
+import styles from './Layout.module.scss';
 import { questrial } from './styles/fonts';
 
 export const metadata = {
     title: 'Home',
-    description: 'Welcome to Emma\'s portfolio',
-}
+    description: "Welcome to Emma's portfolio",
+};
 
-export default function Layout({ children}) {
+export default function Layout({ children }) {
     return (
-        <html lang='en'>
+        <html lang="en">
             <body className={questrial.className}>
                 <header className={styles.header}>
                     <Navigation></Navigation>
-            </header>
+                </header>
                 <main>{children}</main>
             </body>
         </html>
