143036e8568c6853c9519b0a6befa862fd049f2f
emgrey02 <emgrey02@gmail.com>
2025-07-12 15:52:30 -0400

    Update sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -18,5 +18,25 @@ export default function sitemap(): MetadataRoute.Sitemap {
             url: 'https://www.emmagrey.dev/resume',
             lastModified: new Date(),
         },
+        {
+            url: 'https://www.emmagrey.dev/blog/accessiblebuttons',
+            lastModified: new Date(),
+        },
+        {
+            url: 'https://www.emmagrey.dev/blog/fluidtypography',
+            lastModified: new Date(),
+        },
+        {
+            url: 'https://www.emmagrey.dev/blog/rightwaytocss',
+            lastModified: new Date(),
+        },
+        {
+            url: 'https://www.emmagrey.dev/blog/smoothwebanimations',
+            lastModified: new Date(),
+        },
+        {
+            url: 'https://www.emmagrey.dev/blog/themetoggle',
+            lastModified: new Date(),
+        },
     ];
 }
